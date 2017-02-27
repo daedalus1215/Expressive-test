b243206f144d42dfb66dbfacf8d792b023f30fd7
larry <ladams1776@yahoo.com>
2017-02-26 22:45:18 -0500

    db setup for user, one entry

---
+++ src/App/Action/UserActionFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Action;
+
+/**
+ * Description of UserActionFactory
+ *
+ * @author theAdmin
+ */
+class UserActionFactory {
+    //put your code here
+}
